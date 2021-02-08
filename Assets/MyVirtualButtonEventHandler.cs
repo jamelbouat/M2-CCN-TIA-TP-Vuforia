251b73a92609078b0a122cd6b5dc3dbd5ead9bd3
djamel <djamel.bouatmane@etudiant.univ-rennes1.fr>
2021-02-08 06:20:43 +0100

    first commit

---
+++ Assets/MyVirtualButtonEventHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class MyVirtualButtonEventHandler : MonoBehaviour{
+	protected static int count;
+	protected Renderer[] rendererComponents;
+    protected TrackableBehaviour mTrackableBehaviour;
+	public DefaultTrackableEventHandler defaultTrackableEventHandler;
+
+	void Start(){
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		// Register with the virtual buttons TrackableBehaviour
+		var virtualButtonBehaviours = GetComponentsInChildren<VirtualButtonBehaviour>();
+		for (int i = 0; i < virtualButtonBehaviours.Length; ++i){
+			virtualButtonBehaviours[i].RegisterOnButtonPressed(OnButtonPressed);
+			virtualButtonBehaviours[i].RegisterOnButtonReleased(OnButtonReleased);
+		}
+	}
+	
+	void OnButtonPressed(VirtualButtonBehaviour button) {
+		defaultTrackableEventHandler.OnTrackingLost();
+		defaultTrackableEventHandler.OnTrackingFound();
+		Debug.LogFormat("Button Pressed");
+	}
+	
+	void OnButtonReleased(VirtualButtonBehaviour button) {	}
+	
+	void Destroy(){
+		// Register with the virtual buttons TrackableBehaviour
+		var virtualButtonBehaviours = GetComponentsInChildren<VirtualButtonBehaviour>();
+		for (int i = 0; i < virtualButtonBehaviours.Length; ++i){
+			virtualButtonBehaviours[i].UnregisterOnButtonPressed(OnButtonPressed);
+			virtualButtonBehaviours[i].UnregisterOnButtonReleased(OnButtonReleased);
+		}
+	}
+}
---
+++ Assets/SphereAnimationScript.cs
@@ -0,0 +1,38 @@
+﻿ using UnityEngine;
+ 
+ public class SphereAnimationScript : MonoBehaviour{
+	public GameObject astronaut;
+	public GameObject drone;
+	private bool isAstronautFound = false;
+	private bool isDroneFound = false;
+
+	public void OnAstronautFound() {
+		Debug.LogFormat("Found Astronaut");
+		this.isAstronautFound = true;
+	}
+	public void OnAstronautLost() {
+		Debug.LogFormat("Lost Astronaut");
+		this.isAstronautFound = false;
+	}
+	public void OnDroneFound() {
+		Debug.LogFormat("Found Drone");
+		this.isDroneFound = true;
+	}
+	public void OnDroneLost() {
+		Debug.LogFormat("Lost Drone");
+		this.isDroneFound = false;
+	}
+	
+	public void Update() {		
+		Vector3 spherePosition = this.transform.position;
+		Vector3 astronautPosition = astronaut.transform.position;
+		Vector3 dronePosition = drone.transform.position;
+		
+		if (isAstronautFound && isDroneFound){
+			Debug.LogFormat("Targets are found");		
+			this.transform.position = Vector3.MoveTowards(spherePosition, dronePosition, 1f * Time.deltaTime);
+		} else {
+			this.transform.position = Vector3.MoveTowards(spherePosition, astronautPosition, 5f * Time.deltaTime);
+		}
+	}
+ }
